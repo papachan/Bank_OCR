8e8a25693d9131f0044f5f436c9cc9fe533aacdc
papachan <papachan@gmail.com>
2013-04-13 21:46:19 -0500

    writing some tests...

--- src/com/androidda/suite/test/AccountParserTests.java
+++ src/com/androidda/suite/test/AccountParserTests.java
@@ -1,11 +1,31 @@
 package com.androidda.suite.test;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 
-public class AccountParserTests {
+import com.androidda.katabankocr.AccountCheck;
+
+public class AccountParserTests extends TestCase {
 	
 	@Test
 	public void parsingFileTest() {
+	}
+	
+	@Test
+	public void testSumDigits() {
+		String digits = "345882865";
+		this.assertTrue(AccountCheck.checksum(digits));
+		
+	}
+	@Test
+	public void testSumDigitsError() {
+		String digits = "145882865";
+		this.assertFalse(AccountCheck.checksum(digits));
+		
+	}
+	@Test 
+	public void testParseError() {
 		
 	}
 
