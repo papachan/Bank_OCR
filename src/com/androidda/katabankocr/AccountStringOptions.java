11002a3b0da26143ff28f5d542ebdaf68abf26ac
papachan <papachan@gmail.com>
2013-04-18 20:37:45 -0500

    new file AccountStringOptions

---
+++ src/com/androidda/katabankocr/AccountStringOptions.java
@@ -0,0 +1,14 @@
+package com.androidda.katabankocr;
+
+public final class AccountStringOptions{
+	
+	private final String account_option;
+	
+	public AccountStringOptions (String account_option) {
+		this.account_option = account_option;
+	}
+	public String toString() {
+		return account_option;
+	}
+
+}
